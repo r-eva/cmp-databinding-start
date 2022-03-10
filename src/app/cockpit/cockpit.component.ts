d715a40b2d258039562b7a3a00f212e14d9c0f08
reginamandasari <regina.mandasari@accenture.com>
2022-03-10 16:28:02 +0100

    first commit

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: ['./cockpit.component.css']
+})
+export class CockpitComponent implements OnInit {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  // newServerName = '';
+  // newServerContent = '';
+
+  // kelemahannya ini best practicenya nggak bisa dengan gampang redifine sm kita, cuma buat getting input aja. Kl mau redifine pake property binding.
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer(nameInput: HTMLInputElement) {
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    })
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    })
+  }
+
+
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,80 @@
+import { 
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation, 
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ContentChild
+} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // it can be None or Native
+})
+export class ServerElementComponent implements
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
+  @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading', {static: true}) header: ElementRef;
+  @ContentChild('contentParagraph', {static: true}) paragraph: ElementRef
+
+  constructor() {
+    console.log('constructor called!')
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!')
+    console.log(changes)
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!')
+    console.log('Text Content: ' +this.header.nativeElement.textContent)
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent)
+  }
+
+  ngDoCheck(): void {
+      console.log('ngDoCheck called!!')
+  }
+
+  ngAfterContentInit(): void {
+      console.log('ngAfterContentInit called')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewChecked called')
+    console.log('Text Content: ' +this.header.nativeElement.textContent)
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent)
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called')
+  }
+
+}
